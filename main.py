0ac941d6545bc795b2be9ddb6e3a67f22ef0fe44
Sabique-Islam <sabiqueislam7@gmail.com>
2024-09-24 23:03:20 +0530

    Add files via upload

---
+++ main.py
@@ -0,0 +1,15 @@
+# FOR WINDOWS
+import pyttsx3
+if __name__ == '__main__':
+    print("Welcome to RoboSpeaker version 1.0 by Sabique... ")
+    print("Type 'end' to terminate the program.")
+    while True:
+        x = input("Enter what you want the system to speak:- ").lower()
+        if x == "end":
+            print("SEE YOU SOON :)")
+            break
+        engine = pyttsx3.init()
+        engine.say(x)
+        engine.runAndWait()
+
+
