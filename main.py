54ceb883a65763702bf0cde4317ce417f6ef840e
Sabique-Islam <sabiqueislam7@gmail.com>
2024-09-24 23:03:57 +0530

    main.py

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 # FOR WINDOWS
 import pyttsx3
 if __name__ == '__main__':
-    print("Welcome to RoboSpeaker version 1.0 by Sabique... ")
+    print("Welcome to Text to Speech version 1.0 by Sabique... ")
     print("Type 'end' to terminate the program.")
     while True:
         x = input("Enter what you want the system to speak:- ").lower()
